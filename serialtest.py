40f8b96a7fa911a10d7b533e2a6703fb0ad61631
sarojini <35261963+Sarojini96@users.noreply.github.com>
2019-06-23 17:08:45 +0530

    Update serialtest.py

--- serialtest.py
+++ serialtest.py
@@ -1,10 +1,16 @@
 import serial
 
-ser = serial.Serial('com3', 9600) #sets up serial connection (make sure baud rate is correct - matches Arduino)
+#sets up serial connection (make sure baud rate is correct - matches Arduino)
+ser = serial.Serial('com3', 9600) 
+
 while(True):
- data = ser.readline()    #reads until it gets a carriage return. MAKE SURE THERE IS A CARRIAGE RETURN OR IT READS FOREVER
- #sep = data.split()      #splits string into a list at the tabs
+ #reads until it gets a carriage return. MAKE SURE THERE IS A CARRIAGE RETURN OR IT READS FOREVER
+ data = ser.readline()
+ #splits string into a list at the tabs
+ #sep = data.split() 
  print (data)
+ 
+ #create a text document and update the arduino data 
  file = open("records.txt", "a")
  file.writelines("sensor data"+ str(data))
  file.close()
